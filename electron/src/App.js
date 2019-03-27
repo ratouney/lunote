125ce35e07a909bb7b36c864f6a095f4c120c7b9
Jean Pignouf <ratouney1998@gmail.com>
2019-03-27 16:12:36 +0100

    Electron app works

---
+++ electron/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const {app} = window.require('electron').remote;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>CYKA BLYAT</h1>
+          <h2>I fucking hate doing this shit...</h2>
+        </div>
+        <p className="App-intro">
+          <b> Release 4.2.0 </b>
+          Version: {app.getVersion()}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
